c362116a35c08059d750aac80ffdd59546d86585
MikeRD <MikeRD@MikeRD-PC>
2015-06-25 15:42:46 -0400

    Another commit

--- src/javaapplication11/JavaApplication11.java
+++ src/javaapplication11/JavaApplication11.java
@@ -18,6 +18,7 @@ public class JavaApplication11 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println();
+        System.out.println("Just when does it go out??");
     }
     
 }
